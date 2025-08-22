834b9f570d49f8fe44657558e41849afbafe0c28
Pedro Vilcayauri <a20206830@pucp.edu.pe>
2025-08-21 20:28:07 -0500

    feat: La nueva iteración

--- src/main/java/com/pucp/lab020252/Lab020252Application.java
+++ src/main/java/com/pucp/lab020252/Lab020252Application.java
@@ -4,12 +4,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class Lab020252Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Lab020252Application.class, args);
         System.out.println("Hola Mundo");
+		List<String> lista = List.of("Hola", "Mundo", "Frito");
+		for (String item : lista) {
+			System.out.println(item);
+		}
 	}
 
 }
